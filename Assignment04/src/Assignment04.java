66499709ddc2270d789433d2418c97dc86942f97
donovanjmoses <donovanjmoses@macbook-pro.lan>
2021-03-27 10:13:14 -0500

    Refactoring && debugging

--- Assignment04/src/Assignment04.java
+++ Assignment04/src/Assignment04.java
@@ -16,7 +16,8 @@ public class Assignment04 {
 		do {
 			guessCount++;
 			secretNumber = (int)(Math.random()*9000) + 1000;
-			startGame();
+			System.out.println("----- MASTERMIND -----");
+			System.out.printf("Guess the 4 digit number!\n\n");
 			usersGuess = in.nextInt();
 			String secretNumberToString = Integer.toString(secretNumber);
 			String usersGuessToString = Integer.toString(usersGuess);
@@ -32,24 +33,23 @@ public class Assignment04 {
 				matchedNumbers++;}
 			System.out.println("You matched " + matchedNumbers + "\n\n");
 			if (usersGuessToString == secretNumberToString) {
-			System.out.printf("You matched " + matchedNumbers + "\n\n" + "Congratulations! You guessed the right number in " + guessCount + " guesses.\nWould you like to play again? (yes/no)?" );}
+			System.out.printf("You matched " + matchedNumbers + "\n\n" + "Congratulations! You guessed the right number in " + guessCount + " guesses." );}
 			else if (usersGuess == secretNumber) {
-			System.out.printf("You matched " + matchedNumbers + "\n\n" + "Congratulations! You guessed the right number in " + guessCount + " guesses.\nWould you like to play again? (yes/no)?" );}
+			System.out.printf("You matched " + matchedNumbers + "\n\n" + "Congratulations! You guessed the right number in " + guessCount + " guesses." );}
+			
+			/* // working on future enhancements below
 			// the player is prompted to play again by typing 'yes'
 			System.out.println("Would you like to play again? Type 'yes' to continue:");
-			playAgain = in.next();
-			if (playAgain == "yes" || (playAgain == "Yes" || (playAgain == "y") || (playAgain == "Y"))) {
-				startGame();
+			 playAgain = in.next();
+			if ((playAgain == "yes") || (playAgain == "Yes") || (playAgain == "y") || (playAgain == "Y")) {
+				//startGame();
+			} else if ((playAgain == "no") || (playAgain == "No") || (playAgain == "N") || (playAgain == "n")) {
+				System.out.println("Thanks for playing!");
+			} else {
+				System.out.println("Please enter a valid response");
+			} */
 			}
-			}
-			while (usersGuess != secretNumber);
-			
-		
+			while (usersGuess != secretNumber);	
 	}
-	private static void startGame() {
-		// this helper method prompts the user to start the game by entering a 4 digit number
-		System.out.println("----- MASTERMIND -----");
-		System.out.printf("Guess the 4 digit number!\n\n");
-		
-		}
+	
 }
